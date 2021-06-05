e213d1189010423e02fff96cfc3da9411e0e4957
Yannik Jozefowski <yannik.jozefowski@alexanderthamm.com>
2021-06-05 09:44:30 +0200

    first changes

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const key = process.env.API_KEY
 
 const app = express()
 
